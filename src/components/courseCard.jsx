9011b7739d69eb486116f0576e9ee6a2cd42dff0
asib27 <asib.rahman1927@gmail.com>
2024-01-07 18:41:16 +0600

    refactor

--- src/components/courseCard.jsx
+++ src/components/courseCard.jsx
@@ -7,7 +7,7 @@ export default function CourseCard ({ course }) {
     <Link key={course.id} to={`/courses/${course.id}`}>
       <div className='inline-block p-10 basis-1/3 w-96'>
         <div className='rounded overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out'>
-          <img className='w-full' src={course.image} alt='Course Image' />
+          <img className='w-full p-5' src={course.image} alt='Course Image' />
           <AuthorList className='px-6 py-2'>{course.authors}</AuthorList>
           <div className='px-6 py-4'>
             <div className='font-bold text-xl mb-2'>{course.name}</div>
