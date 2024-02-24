5ac71dfd67321598ea96776b3ee8380676fa7811
asib27 <asib.rahman1927@gmail.com>
2024-02-24 23:06:27 +0600

    add quiz starting page

--- src/routes/quizes.jsx
+++ src/routes/quizes.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from 'react-router-dom'
 import { getQuizStat, getQuizes } from '../services/quizService'
 import { useState } from 'react'
 import QuizQuestion from '../pages/quizQuestion'
+import QuizStartingView from '../components/quiz/quizStartingView'
 
 // TODO: implement finish quiz
 
@@ -83,32 +84,4 @@ export default function Quizes () {
 
     )
   }
-}
-function QuizStartingView ({ quizStat, setQuizViewState }) {
-  return (
-    <div className='p-10'>
-      <p className='text-3xl'>{quizStat.name}</p>
-
-      <div className='mt-10 grid grid-cols-2'>
-        <p className='text-xl'>Max Score Available :</p>
-        <p className='text-xl'>{quizStat.score}</p>
-
-        <p className='text-xl'>Max XP Available :</p>
-        <p className='text-xl'>{quizStat.xp}</p>
-
-        <p className='text-xl'>Highest Score :</p>
-        <p className='text-xl'>{quizStat.highest_score}</p>
-
-        <p className='text-xl'>Your Highest Score :</p>
-        <p className='text-xl'>{quizStat.my_highest_score}</p>
-      </div>
-
-      <button
-        onClick={() => { setQuizViewState(1) }}
-        className='mt-10 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
-      >
-        Start Quiz
-      </button>
-    </div>
-  )
 }
